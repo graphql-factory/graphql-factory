6b0527c9a886a8f3787ad4bf17844bccc0758598
Branden Horiuchi <bhoriuchi@gmail.com>
2016-06-05 00:33:58 -0700

    * refactored schema creation code * added enum and input type suport * added more test cases * made make operation return a function for each schema that takes a query as its argument

--- example/example.js
+++ example/example.js
@@ -1,8 +1,12 @@
 import _ from 'lodash'
 import * as graphql from 'graphql'
+import CustomGraphQLDateType from 'graphql-custom-datetype'
 import rethinkdbdash from 'rethinkdbdash'
 let r = rethinkdbdash()
 let factory = require('../index')(graphql)
+factory.registerTypes({
+  DateTime: CustomGraphQLDateType
+})
 
 /* make sure tables exist */
 function createTable (db, name) {
@@ -19,10 +23,81 @@ _.forEach(tables, function (type) {
   if (type.db && type.table) createTable(type.db, type.table)
 })
 
+//  get user list
+let getUsers = function () {
+  return r.db(tables.user.db).table(tables.user.table).run()
+}
+
+//  purge users
+let purgeUsers = function () {
+  return r.db(tables.user.db).table(tables.user.table).delete().run().then(function () {
+    return 200
+  }).catch(function (err) {
+    return 500
+  })
+}
+
+//  create function
+let createUser = function (obj, args) {
+  let changeLog = args.changeLog || { user: 'SYSTEM', message: 'CREATED RECORD' }
+  _.merge(changeLog, { date: new Date(), type: 'CREATE' })
+  return r.db(tables.user.db).table(tables.user.table)
+    .insert({
+      _metadata: {
+        recordId: r.uuid(),
+        version: null,
+        validFrom: null,
+        validTo: null,
+        changeLog: [changeLog]
+      },
+      id: r.uuid(),
+      firstName: args.firstName,
+      lastName: args.lastName,
+      email: args.email
+    }, {
+      returnChanges: true
+    })
+    .run()
+    .then(function (result) {
+      if (result.changes.length > 0) return result.changes[0].new_val
+      return result
+    })
+}
+
 
 let schema = {
   types: {
+    EnumChangeLogTypes: {
+      $$type: 'Enum',
+      CREATE: 'CREATE',
+      UPDATE: 'UPDATE',
+      BRANCH: 'BRANCH',
+      FORK: 'FORK',
+      MERGE: 'MERGE',
+      PUBLISH: 'PUBLISH',
+      VERSION: 'VERSION',
+      INFO: 'INFO'
+    },
+    _ChangeLog: {
+      date: { type: 'DateTime', nullable: true },
+      type: { type: 'EnumChangeLogTypes', nullable: true },
+      user: { type: 'String', nullable: true},
+      message: { type: 'String', nullable: true }
+    },
+    _ChangeLogInput: {
+      $$type: 'Input',
+      user: { type: 'String', nullable: true },
+      message: { type: 'String', nullable: true }
+    },
+    _VersionMetadata: {
+      recordId: { type: 'String', nullable: true },
+      version: { type: 'String', nullable: true },
+      validFrom: { type: 'DateTime', nullable: true },
+      validTo: { type: 'DateTime', nullable: true },
+      changeLog: { type: ['_ChangeLog'], nullable: true }
+    },
     User: {
+      _metadata: { type: '_VersionMetadata', nullable: true },
       id: { type: 'String', primary: true },
       firstName: { type: 'String' },
       lastName: { type: 'String' },
@@ -34,9 +109,7 @@ let schema = {
       query: {
         users: {
           type: ['User'],
-          resolve: (root, args) => {
-            return r.db(tables.user.db).table(tables.user.table).run()
-          }
+          resolve: getUsers
         }
       },
       mutation: {
@@ -45,53 +118,61 @@ let schema = {
           args: {
             firstName: { type: 'String' },
             lastName: { type: 'String' },
-            email: { type: 'String', nullable: true }
+            email: { type: 'String', nullable: true },
+            changeLog: { type: '_ChangeLogInput', nullable: true }
           },
-          resolve: (obj, args, source, fieldASTs) => {
-            return r.db(tables.user.db).table(tables.user.table)
-              .insert({
-                id: r.uuid(),
-                firstName: args.firstName,
-                lastName: args.lastName,
-                email: args.email
-              }, {
-                returnChanges: true
-              })
-              .run()
-              .then(function (result) {
-                if (result.changes.length > 0) return result.changes[0].new_val
-                return result
-            })
-          }
+          resolve: createUser
+        },
+        purge: {
+          type: 'Int',
+          resolve: purgeUsers
         }
       }
     }
   }
 }
 
-let defs = factory.make(schema)
+let lib = factory.make(schema)
 
-/*
-let s = new graphql.GraphQLSchema({
-  query: new graphql.GraphQLObjectType({
-    name: 'UserQueryType',
-    fields: {
-      users: {
-        type: new graphql.GraphQLList(defs.types.User),
-        resolve: (root, args) => {
-          return r.db(tables.user.db).table(tables.user.table).run()
-        }
+let testCreateGQL = `mutation Mutation {
+  create(
+    firstName: "john",
+    lastName: "doe",
+    email: "jdoe@x.com"
+  )
+  {
+    id, firstName, lastName, email,
+    _metadata {
+      recordId, version, validFrom, validTo,
+      changeLog {
+        date, type, user, message
       }
     }
-  })
-})
-*/
+  }
+}`
 
-graphql.graphql(defs.schemas.Users, '{ users { id, firstName, lastName, email } }').then(function (result) {
-  _.forEach(result.errors, function (e, i) {
-    result.errors[i] = e.message
-  })
-  console.log(JSON.stringify(result, null, '  '))
-})
+let testPurgeGQL = `mutation Mutation {
+  purge
+}`
 
-// console.log(d.make(schema))
+let testGetGQL = `{
+  users {
+    id, firstName, lastName, email,
+    _metadata {
+      recordId, version, validFrom, validTo,
+      changeLog {
+        date, type, user, message
+      }
+    }
+  }
+}`
+
+// lib.Users(testCreateGQL)
+// lib.Users(testPurgeGQL)
+lib.Users(testGetGQL)
+  .then(function (result) {
+    _.forEach(result.errors, function (e, i) {
+      result.errors[i] = e.message
+    })
+    console.log(JSON.stringify(result, null, '  '))
+  })
