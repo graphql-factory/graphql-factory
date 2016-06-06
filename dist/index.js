f698b07f45b941e5a43df380059d54a6908b72ab
Branden Horiuchi <bhoriuchi@gmail.com>
2016-06-06 11:33:58 -0700

    * removing dist from npmignore and actually pushing the dist folder

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _lib = require('./lib');
+
+var _lib2 = _interopRequireDefault(_lib);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = _lib2.default;
