8497e16019f58a6dc6a90d0421d96bd2d2598a18
Branden Horiuchi <bhoriuchi@gmail.com>
2016-06-16 07:19:52 -0700

    * fixing path to tests

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 var chai = global.chai = require('chai')
 var rewire = global.rewire = require('rewire')
 var graphql = global.graphql = require('graphql')
-var private = global.private = rewire('../dist')
-var factory = global.factory = require('../dist')(graphql)
+var private = global.private = rewire('../index')
+var factory = global.factory = require('../index')(graphql)
 var expect = global.expect = chai.expect
 
 var unitTests = require('./unit')
